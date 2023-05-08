029b5a616ce94877f1dae3ee73ce5c4032f19557
jjw0071 <jjw0071@naver.com>
2023-05-08 13:35:59 +0900

    연습문제

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,21 @@ import java.util.ArrayList;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
+
+
+
+    }
+
+    public static double circleArea(){
+        final double PI = 3.14;
+        int radius = 3;
+        double area = PI*radius*radius;
+        System.out.println("원의 면적은 "+area+"입니다.");
+        return area;
+    }
+
+    public static void sample(){
         String a = "write once, run anywhere";
         if (a.contains("wife")) {
             System.out.println("wife");
@@ -14,8 +29,5 @@ public class HelloWorld {
         } else {
             System.out.println("none");
         }
-
-
-
     }
 }
