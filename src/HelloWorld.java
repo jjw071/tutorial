239701cff6902cabee2dde4f9447b74109348d11
jjw0071 <jjw0071@naver.com>
2023-05-08 12:34:22 +0900

    tutorial

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!");
+        String a = "write once, run anywhere";
+        if (a.contains("wife")) {
+            System.out.println("wife");
+        } else if (a.contains("once") && !a.contains("run")) {
+            System.out.println("once");
+        } else if (!a.contains("everywhere")) {
+            System.out.println("everywhere");
+        } else if (a.contains("anywhere")) {
+            System.out.println("anywhere");
+        } else {
+            System.out.println("none");
+        }
+
+
+
     }
 }
