b79ad62be77e02bbc73e75c9bba1837454ae6ab1
jjw0071 <jjw0071@naver.com>
2023-05-08 13:37:41 +0900

    연습문제 3

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -30,4 +30,16 @@ public class HelloWorld {
             System.out.println("none");
         }
     }
+
+    public static void addthree(){
+        int result = 0;
+        int i = 1;
+        while (i <= 1000) {
+            if (i % 3 == 0) {  // 3으로 나누어 떨어지는 수는 3의 배수
+                result += i;
+            }
+            i += 1;
+        }
+        System.out.println(result);  // 166833 출력
+    }
 }
